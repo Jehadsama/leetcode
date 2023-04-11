03c6dbf9bd90819ab291a6aa21dc6c3e9a218dca
jehadsama <ayumijehad@qq.com>
2023-04-11 21:51:42 +0800

    feat(26.删除有序数组中的重复项)

---
+++ 26.删除有序数组中的重复项.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=javascript
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length === 0) return 0;
+
+  let left = (right = 0);
+
+  for (let right = 1; right < nums.length; right++) {
+    if (nums[left] !== nums[right]) {
+      left++;
+      nums[left] = nums[right];
+    }
+  }
+  return left + 1;
+};
+// @lc code=end
