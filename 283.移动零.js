3f2f5a54baa8aec4c9450aeec58ba89657dfce34
jehadsama <ayumijehad@qq.com>
2023-04-18 11:33:44 +0800

    feat(283.移动零)

--- 283.移动零.js
+++ 283.移动零.js
@@ -9,17 +9,19 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
+
 var moveZeroes = function (nums) {
   let left = (right = 0);
-  while (nums[right] !== undefined) {
-    if (nums[right] !== 0) {
+  while (right < nums.length) {
+    if (nums[right] > 0) {
       nums[left] = nums[right];
-      if (left !== right) {
-        nums[right] = 0;
-      }
       left++;
     }
     right++;
   }
+  for (let i = left; i < nums.length; i++) {
+    nums[i] = 0;
+  }
+  return nums;
 };
 // @lc code=end
