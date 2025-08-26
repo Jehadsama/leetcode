f853d92062b89ec1e8cb94b482cb21c0d07dfa70
wxlijiahang <wxlijiahang@gfpartner.com.cn>
2025-08-26 22:45:23 +0800

    feat(算法/希尔排序.js)

---
+++ 算法/希尔排序.js
@@ -0,0 +1,27 @@
+// 希尔排序（Shell Sort）基本思想：
+// 通过设定不同的间隔（gap），将数组分组进行插入排序，然后逐步缩小间隔直至为 1，最终完成整个数组的排序。
+
+function shellSort(arr) {
+  let gap = Math.floor(arr.length / 2);
+
+  while (gap > 0) {
+    for (let i = gap; i < arr.length; i++) {
+      let temp = arr[i];
+      let j = i;
+
+      while (j >= gap) {
+        if (arr[j - gap] > temp) {
+          arr[j] = arr[j - gap];
+          j -= gap;
+        }
+      }
+      arr[j] = temp;
+    }
+
+    gap = Math.floor(gap / 2);
+  }
+
+  return arr;
+}
+
+console.log(shellSort([3, 2, 1]));
